962fc450aaa8aff20a0812e8d559b7c6d1095eba
om <awsdevopsom@gmail.com>
2024-12-23 23:24:41 +0530

    om

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -1,1 +1,7 @@
 vpc_cidr_block = "10.10.0.0/16"
+env = "dev"
+tags = {
+    company_name = "xyz"
+    bu_unit = "expense"
+    project = " finance"
+}
--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
 module "VPC" {
     source = "./module/VPC"
     vpc_cidr_block = var.vpc_cidr_block
+    env = var.env
+    tags = var.tags
+   
   }
+
+  resource "null_resource" "null" {}
