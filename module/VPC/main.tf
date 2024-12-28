3ffb530edbd86cb749e19e8391ce8bdb11d02b38
om <awsdevopsom@gmail.com>
2024-12-28 14:42:54 +0530

    up

--- module/VPC/main.tf
+++ module/VPC/main.tf
@@ -2,3 +2,12 @@ resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr_block
   tags = merge(var.tags, {Name = var.env} )
 }
+
+resource "aws_subnet" "main" {
+  count = length(var.public_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnets[count.index]
+  availability_zone = var.azs[count.index]
+  tags = merge(var.tags, {Name = "public-subnet"} )
+}
+
