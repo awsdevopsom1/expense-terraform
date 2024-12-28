40696f85b9933178446d0097e4bb48551dadb1ef
om <awsdevopsom@gmail.com>
2024-12-28 15:26:44 +0530

    om

--- module/VPC/main.tf
+++ module/VPC/main.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "main" {
   tags = merge(var.tags, {Name = var.env} )
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "public" {
   count = length(var.public_subnets)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnets[count.index]
@@ -11,3 +11,26 @@ resource "aws_subnet" "main" {
   tags = merge(var.tags, {Name = "public-subnet"} )
 }
 
+resource "aws_subnet" "web" {
+  count = length(var.web_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.web_subnets[count.index]
+  availability_zone = var.azs[count.index]
+  tags = merge(var.tags, {Name = "web-subnet"} )
+}
+
+resource "aws_subnet" "db" {
+  count = length(var.db_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.db_subnets[count.index]
+  availability_zone = var.azs[count.index]
+  tags = merge(var.tags, {Name = "db-subnet"} )
+}
+
+resource "aws_subnet" "app" {
+  count = length(var.app_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.app_subnets[count.index]
+  availability_zone = var.azs[count.index]
+  tags = merge(var.tags, {Name = "app-subnet"} )
+}
